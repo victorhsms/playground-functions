3dd2ce0d7d775c283481bdb570b8af82758e83ce
Victor Hugo <victorhsmsantos@gmail.com>
2021-11-11 18:15:58 -0300

    decimo requisito completo

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,16 @@
 // Desafio 10
-function techList() {
+function techList(techs, name) {
   // seu código aqui
+  if (techs.length == 0) {
+    return 'Vazio!'
+  }
+  techs.sort()
+  //Pesquisando por "organizar o array em ordem alfabetica javascript" no Google, encontrei a explicação do método '.sort()' no Javascript no link: https://ricardo-reis.medium.com/o-m%C3%A9todo-sort-do-array-javascript-482576734e0a#:~:text=Para%20ordenar%20esse%20array%20em,para%20o%20m%C3%A9todo%20sort()%20 e usei no código acima.
+  let saida = [];
+  for (let tech in techs) {
+    saida[tech] = {'tech':techs[tech],'name':name};
+  }
+  return saida;
 }
 
 // Desafio 11
